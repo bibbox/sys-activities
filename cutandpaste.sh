13fa4335f4ce5aca41b7d6125c66342acca042fd
lomadi <lomadi@mac.com>
2016-11-08 14:17:18 +0100

    prepare automatic installation

---
+++ cutandpaste.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/bibbox/sys-activities.git 
+
+git pull
+
+git add .
+git commit -m "update from development server"
+git push -u origin master
+
+git remote add origin https://github.com/bibbox/sys-activities.git 
+
---
+++ mk-data-volume-folders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+mkdir -p datavolume/redis/data
+mkdir -p datavolume/orientdb/config
+mkdir -p datavolume/orientdb/databases
+mkdir -p datavolume/orientdb/backup
