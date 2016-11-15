130608563099caec6140173061b9bd1a92bd522a
lomadi <lomadi@mac.com>
2016-11-15 13:33:49 +0100

    V0.99

--- app/run_web.sh
+++ app/run_web.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-pip install -r /app/requirements.txt
 cd /app/activities/ && gunicorn --reload -b 0.0.0.0:8000 app:app
--- cutandpaste.sh
+++ cutandpaste.sh
@@ -8,3 +8,6 @@ git push -u origin master
 
 git remote add origin https://github.com/bibbox/sys-activities.git 
 
+
+http://127.0.0.1:8050/activities/api/v1.0/activities
+http://127.0.0.1:5000/activities/api/v1.0/activities
